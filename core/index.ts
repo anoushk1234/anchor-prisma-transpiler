bde367f96b0dc70f14dca09a6b9a66ae2c07bb02
anoushk1234 <anoushka1828@gmail.com>
2022-08-25 23:51:09 +0530

    sqlana is live

---
+++ core/index.ts
@@ -0,0 +1,164 @@
+import { AnchorTypes } from "./mappings";
+import fs from "fs";
+
+let idlSchema = {
+  accounts: JSON.parse(fs.readFileSync("./idl.json", "utf8")).accounts,
+  types: JSON.parse(fs.readFileSync("./idl.json", "utf8")).types,
+};
+console.log("hijoiw");
+let currentPrismaSchema = fs.readFileSync("./prisma/schema.prisma", "utf8");
+
+console.log("blah");
+Object.keys(idlSchema).map((key) => {
+  let prismaschema = "";
+  try {
+    if (key === "accounts") {
+      const types = idlSchema.accounts;
+      for (let i in types) {
+        let data;
+        const type = types[i];
+        // console.log(type.name.includes("Input"), type.name);
+        if (type.name.toLowerCase().includes("input")) continue; // just for custom input structs
+        if (type.type.kind === "enum") {
+          data = `
+      enum ${type.name}{
+          ${
+            //@ts-ignore
+            type?.type?.variants?.map((v) => v.name).join("\n")
+          }
+          }
+      `;
+        } else if (type.type.kind === "struct") {
+          data = `
+      model ${type.name}{
+          ${type?.type?.fields
+            ?.map((f: any, i: number) => {
+              // console.log(f.type);
+              let field = "";
+              if (i === 0) {
+                field = "pubkey String @id \n";
+              }
+              try {
+                if (Object.keys(f.type).includes("vec")) {
+                  let typefield: AnchorTypes | string =
+                    typeof f.type == "string"
+                      ? AnchorTypes.string
+                      : typeof f.type.vec === "string"
+                      ? AnchorTypes.publicKey
+                      : f.type.vec.defined;
+                  if (f.type.vec.defined) {
+                    field += f.name.concat(
+                      ` ${typefield}[] \n ${f.name}Id String`
+                    );
+                  } else {
+                    field += f.name.concat(` ${typefield}[]`);
+                  }
+                  // console.log(field, "field");
+                } else if (Object.keys(f.type).includes("defined")) {
+                  let typefield: AnchorTypes | string =
+                    typeof f.type == "string"
+                      ? AnchorTypes.string
+                      : f.type.defined;
+                  if (f.type.defined) {
+                    field += f.name.concat(
+                      ` ${typefield} \n ${f.name}Id String`
+                    );
+                    // console.log("this", field);
+                  } else {
+                    field += f.name.concat(` ${typefield}`);
+                  }
+                } else {
+                  let typefield: AnchorTypes | string | null =
+                    typeof f.type == "string" ? AnchorTypes.string : null;
+                  field += f.name.concat(` ${typefield}`);
+                }
+              } finally {
+                // console.log("f2", field);
+                return field;
+              }
+            })
+            .join("\n")}
+      }
+      `;
+        }
+
+        if (data) prismaschema += data;
+      }
+    } else if (key === "types") {
+      const types = idlSchema.types;
+      for (let i in types) {
+        let data;
+        const type = types[i];
+        // console.log(type.name.includes("Input"), type.name);
+        if (type.name.toLowerCase().includes("input")) continue; // just for custom input structs
+        if (type.type.kind === "enum") {
+          data = `
+      enum ${type.name}{
+          ${
+            //@ts-ignore
+            type?.type?.variants?.map((v) => v.name).join("\n")
+          }
+          }
+      `;
+        } else if (type.type.kind === "struct") {
+          data = `
+      model ${type.name}{
+          ${type?.type?.fields
+            ?.map((f: any, i: number) => {
+              // console.log(f.type);
+              let field = "";
+              if (i === 0) {
+                field = `${type.name}Id String @id @default(uuid()) \n`;
+              }
+              try {
+                if (Object.keys(f.type).includes("vec")) {
+                  let typefield: AnchorTypes | string =
+                    typeof f.type == "string"
+                      ? AnchorTypes.string
+                      : typeof f.type.vec === "string"
+                      ? AnchorTypes.publicKey
+                      : f.type.vec.defined;
+                  if (f.type.vec.defined) {
+                    field += f.name.concat(
+                      ` ${typefield}[] \n ${f.name}Id String`
+                    );
+                    // console.log("this", field);
+                  } else {
+                    field += f.name.concat(` ${typefield}[]`);
+                  }
+                  // console.log(field, "field");
+                } else if (Object.keys(f.type).includes("defined")) {
+                  let typefield: AnchorTypes | string =
+                    typeof f.type == "string"
+                      ? AnchorTypes.string
+                      : f.type.defined;
+                  if (f.type.defined) {
+                    field += f.name.concat(
+                      ` ${typefield} \n ${f.name}Id String`
+                    );
+                  } else {
+                    field += f.name.concat(` ${typefield}`);
+                  }
+                } else {
+                  let typefield: AnchorTypes | string | null =
+                    typeof f.type == "string" ? AnchorTypes.string : null;
+                  field += f.name.concat(` ${typefield}`);
+                }
+              } finally {
+                // console.log("f2", field);
+                return field;
+              }
+            })
+            .join("\n")}
+      }
+      `;
+        }
+
+        if (data) prismaschema += data;
+      }
+    }
+  } finally {
+    let finalSchema = currentPrismaSchema.concat(`\n ${prismaschema}`);
+    fs.writeFileSync("./prisma/schema.prisma", finalSchema);
+  }
+});
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Sqlana ⚡️
+
+## Introduction
+
+Let's say you wrote a solana program using anchor, now you want to build a dapp, you have two options either use an indexer and rely on your contract for data or have a database and api. Both approaches require a database schema~
+
+## Introducing _Sqlana_ ⚡️
+
+A transpiler for all our schemas.<br>
+Anchor program IDL -> Prisma Schema △ <br>
+Anchor program IDL -> SQL Schema 💿 <br>
+Anchor program IDL -> Graphql Schema 🕸 <br>
+
+Currently this works with Prisma Schema but we want to add support for more.
+
+# Quickstart
+
+- `yarn install`
+- Add the idl json file in idl.json
+- `yarn setup` (only for first time otherwise yarn start)
+
+Support this project on anoushk.sol or [wagmi.bio/anoushk](https://wagmi.bio/anoushk)
